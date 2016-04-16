ff3412c52cdd8d7b4606835a22ed3bd8a9e60ebc
Luke <lukemorton.dev@gmail.com>
2016-04-16 11:51:58 +0100

    Add #set_completed_at

--- lib/checkout/steps/complete_step.rb
+++ lib/checkout/steps/complete_step.rb
@@ -1,6 +1,7 @@
 module Checkout
   class CompleteStep < Step
     steps :ensure_incomplete,
+          :set_completed_at,
           :update_step
 
     private
@@ -9,6 +10,10 @@ module Checkout
       if self[:name] == state[:step]
         raise AlreadyCompleteError.new(state)
       end
+    end
+
+    def set_completed_at(state)
+      state.merge(completed_at: Time.now)
     end
 
     class AlreadyCompleteError < RuntimeError; end
--- spec/checkout/steps/complete_step_spec.rb
+++ spec/checkout/steps/complete_step_spec.rb
@@ -1,6 +1,13 @@
 describe Checkout::CompleteStep do
-  context 'when processing already completed step' do
-    subject { Checkout::CompleteStep.new.process({ step: :complete }) }
-    it { expect { subject }.to raise_error(Checkout::CompleteStep::AlreadyCompleteError) }
+  context 'when processing checkout' do
+    context 'and order incomplete' do
+      subject { Checkout::CompleteStep.new.process({ step: :new }) }
+      it { is_expected.to include(:completed_at) }
+    end
+
+    context 'and order already completed' do
+      subject { Checkout::CompleteStep.new.process({ step: :complete }) }
+      it { expect { subject }.to raise_error(Checkout::CompleteStep::AlreadyCompleteError) }
+    end
   end
 end
