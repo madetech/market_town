5dd1213826b5e05a64359e1bbf416de53440a0e1
Luke <lukemorton.dev@gmail.com>
2016-04-17 12:21:50 +0100

    Add #validate_shipments to DeliveryStep

--- checkout/lib/market_town/checkout/steps/delivery_step.rb
+++ checkout/lib/market_town/checkout/steps/delivery_step.rb
@@ -2,8 +2,10 @@ module MarketTown
   module Checkout
     class DeliveryStep < Step
       class InvalidDeliveryAddress < RuntimeError; end
+      class CannotFulfilShipmentsError < RuntimeError; end
 
-      steps :ensure_delivery_address
+      steps :ensure_delivery_address,
+            :validate_shipments
 
       private
 
@@ -11,6 +13,14 @@ module MarketTown
         Address.validate!(state[:delivery_address])
       rescue Address::InvalidError => e
         raise InvalidDeliveryAddress.new(e.data)
+      end
+
+      def validate_shipments(state)
+        unless deps.fulfilment.can_fulfil_shipments?(state[:shipments])
+          raise CannotFulfilShipmentsError.new(state[:shipments])
+        end
+      rescue MissingDependency
+        add_warning(state, :cannot_validate_shipments)
       end
     end
   end
--- checkout/spec/checkout/steps/delivery_step_spec.rb
+++ checkout/spec/checkout/steps/delivery_step_spec.rb
@@ -1,6 +1,10 @@
 module MarketTown::Checkout
   describe DeliveryStep do
-    let(:deps) { Dependencies.new }
+    let(:fulfilment) { double(can_fulfil_shipments?: true) }
+
+    let(:deps) { Dependencies.new(fulfilment: fulfilment,
+                                  logger: double(warn: nil)) }
+
     let(:steps) { DeliveryStep.new(deps) }
 
     let(:mock_address) do
@@ -16,6 +20,18 @@ module MarketTown::Checkout
         subject { steps.process(delivery_address: mock_address) }
 
         it { is_expected.to include(:delivery_address) }
+
+        context 'and fulfilment missing' do
+          let(:fulfilment) { nil }
+
+          it { is_expected.to include(:warnings) }
+        end
+
+        context 'and cannot fulfil shipments' do
+          let(:fulfilment) { double(can_fulfil_shipments?: false) }
+
+          it { expect { subject }.to raise_error(DeliveryStep::CannotFulfilShipmentsError) }
+        end
       end
 
       context 'and delivery address missing' do
