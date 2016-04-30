16678642412fb38523ab553b358bf22ff8772887
Luke <lukemorton.dev@gmail.com>
2016-04-30 18:11:21 +0100

    Document ::Step

--- checkout/lib/market_town/checkout/steps/step.rb
+++ checkout/lib/market_town/checkout/steps/step.rb
@@ -1,6 +1,92 @@
 module MarketTown
   module Checkout
+    # Extended by all steps provided by {MarketTown::Checkout}.
+    #
+    # ## Creating your own {Step}
+    #
+    # If you wish to create your own step then you can extend {Step} or you can
+    # create a class that looks like step.
+    #
+    # ### Extending {Step}
+    #
+    # ``` ruby
+    # class MyStep < MarketTown::Checkout::Step
+    #   steps :a_step,
+    #         :a_second_step
+    #
+    #   protected
+    #
+    #   def a_step(state)
+    #   end
+    #
+    #   def a_second_step(state)
+    #   end
+    # end
+    # ```
+    #
+    # ### Duck typing {Step}
+    #
+    # ``` ruby
+    # class MyCustomStep
+    #   def initialize(dependencies)
+    #   end
+    #
+    #   def process(state)
+    #   end
+    # end
+    # ```
+    #
+    # ## Extending an existing step
+    #
+    # You may want to alter the behaviour of a {Step}. You can do this quite
+    # easily by extending the class.
+    #
+    # In the example below we'll replace the behaviour of {AddressStep} to
+    # allow the use of delivery address as billing address instead of the
+    # default behaviour that does the opposite.
+    #
+    # ``` ruby
+    # class MyAddressStep < MarketTown::Checkout::AddressStep
+    #   # Here we override the step order as defined in AddressStep. We
+    #   # also replace #use_billing_address_as_delivery_address with
+    #   # #use_delivery_address_as_billing_address.
+    #   #
+    #   steps :validate_delivery_address,
+    #         :use_delivery_address_as_billing_address,
+    #         :validate_billing_address,
+    #         :ensure_delivery,
+    #         :store_addresses,
+    #         :propose_shipments,
+    #         :finish_address_step
+    #
+    #   protected
+    #
+    #   def use_delivery_address_as_billing_address(state)
+    #     if state[:use_delivery_address] == true
+    #       state.merge(billing_address: state[:delivery_address])
+    #     end
+    #   end
+    # end
+    # ```
+    #
+    #
     class Step
+      # Set steps for a subclass of {Step}.
+      #
+      # @example
+      #   class MyStep < MarketTown::Checkout::Step
+      #     steps :a_step,
+      #           :a_second_step
+      #
+      #     protected
+      #
+      #     def a_step(state)
+      #     end
+      #
+      #     def a_second_step(state)
+      #     end
+      #   end
+      #
       def self.steps(*steps)
         if steps.empty?
           @steps
@@ -11,11 +97,17 @@ module MarketTown
 
       attr_reader :meta, :deps
 
+      # Setup step meta object.
+      #
+      # @param [Dependencies]
+      #
       def initialize(dependencies = Dependencies.new)
         @meta = { name: name_from_class }
         @deps = dependencies
       end
 
+      # Process each sub-step that makes up step.
+      #
       def process(state)
         self.class.steps.reduce(state) do |state, step|
           send(step, state) || state
