0724353a01afed38ea8da53a2a5f114771389cec
Luke <lukemorton.dev@gmail.com>
2016-05-03 14:31:37 +0100

    Add method param docs

--- checkout/lib/market_town/checkout/contracts/address_storage.rb
+++ checkout/lib/market_town/checkout/contracts/address_storage.rb
@@ -2,9 +2,16 @@ module MarketTown
   module Checkout
     module Contracts
       class AddressStorage
+        # @param [Hash] state
+        # @option state [Hash] :order object by which to find addresses
+        #
         def load_default(state)
         end
 
+        # @param [Hash] state
+        # @option state [Hash] :billing_address as per {Address#validate!}
+        # @option state [Hash] :delivery_address as per {Address#validate!}
+        #
         def store(state)
         end
       end
