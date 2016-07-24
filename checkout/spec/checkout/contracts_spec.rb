13abd4ba3c397c7d0f0dbb18018158d9dc3b4fc1
Luke <lukemorton.dev@gmail.com>
2016-07-24 18:26:54 +0100

    Add contract for Spree::UserAddressStorage

--- checkout/spec/checkout/contracts_spec.rb
+++ checkout/spec/checkout/contracts_spec.rb
@@ -1,37 +1,41 @@
-module MarketTown::Checkout::Contracts
-  describe UserAddressStorage do
-    it_behaves_like 'UserAddressStorage'
-  end
+module MarketTown::Checkout
+  describe Contracts do
+    let(:state) { {} }
 
-  describe Credit do
-    it_behaves_like 'Credit'
-  end
+    describe Contracts::UserAddressStorage do
+      it_behaves_like 'UserAddressStorage'
+    end
 
-  describe Finish do
-    it_behaves_like 'Finish'
-  end
+    describe Contracts::Credit do
+      it_behaves_like 'Credit'
+    end
 
-  describe Fulfilments do
-    it_behaves_like 'Fulfilments'
-  end
+    describe Contracts::Finish do
+      it_behaves_like 'Finish'
+    end
 
-  describe Notifications do
-    it_behaves_like 'Notifications'
-  end
+    describe Contracts::Fulfilments do
+      it_behaves_like 'Fulfilments'
+    end
 
-  describe Order do
-    it_behaves_like 'Order'
-  end
+    describe Contracts::Notifications do
+      it_behaves_like 'Notifications'
+    end
 
-  describe Payments do
-    it_behaves_like 'Payments'
-  end
+    describe Contracts::Order do
+      it_behaves_like 'Order'
+    end
 
-  describe Promotions do
-    it_behaves_like 'Promotions'
-  end
+    describe Contracts::Payments do
+      it_behaves_like 'Payments'
+    end
 
-  describe Tax do
-    it_behaves_like 'Tax'
+    describe Contracts::Promotions do
+      it_behaves_like 'Promotions'
+    end
+
+    describe Contracts::Tax do
+      it_behaves_like 'Tax'
+    end
   end
 end
