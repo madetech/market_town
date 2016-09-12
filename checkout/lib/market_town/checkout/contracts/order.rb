3ec4f458874cfad45b49f4feec699c3a8929802e
Luke <lukemorton.dev@gmail.com>
2016-09-12 12:26:36 +0100

    Add missing Order#set_addresses contract

--- checkout/lib/market_town/checkout/contracts/order.rb
+++ checkout/lib/market_town/checkout/contracts/order.rb
@@ -6,10 +6,18 @@ module MarketTown
           true
         end
 
+        def set_addresses(state)
+        end
+
         shared_examples_for 'Order' do
           context '#has_line_items?' do
             subject { described_class.new.has_line_items?({}) }
             it_behaves_like 'a boolean query method'
+          end
+
+          context '#set_addresses' do
+            subject { described_class.new.set_addresses({}) }
+            it_behaves_like 'a command method'
           end
         end
       end
